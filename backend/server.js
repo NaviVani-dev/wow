96fbb251012516e1f40587a9bf83fc1180620bbc
Raulito2504 <raulariasrafael@gmail.com>
2026-09-13 21:38:10 -0500

    feat: conecta rutas de puntuaciones al servidor

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import 'dotenv/config';
 import jugadoresRoutes from './routes/jugadores.routes.js';
 import videojuegosRoutes from './routes/videojuegos.routes.js';
+import puntuacionesRoutes from './routes/puntuaciones.routes.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/jugadores', jugadoresRoutes);
 app.use('/videojuegos', videojuegosRoutes);
+app.use('/puntuaciones', puntuacionesRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));

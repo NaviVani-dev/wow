fdcb7e0f7b1004526d7298f8b5faf70458400d05
Raulito2504 <raulariasrafael@gmail.com>
2026-09-13 20:43:35 -0500

    feat: añade ruta GET para buscar jugadores

--- backend/routes/jugadores.routes.js
+++ backend/routes/jugadores.routes.js
@@ -5,6 +5,7 @@ import validarCampos from '../middlewares/validaciones.js';
 const router = Router();
 
 router.get('/', jugadoresController.consultar);
+router.get('/buscar', jugadoresController.buscar);
 router.post('/', validarCampos(['nombre', 'gamertag', 'correo']), jugadoresController.registrar);
 
 export default router;

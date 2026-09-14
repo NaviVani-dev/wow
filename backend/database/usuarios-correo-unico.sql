8a63f7a87061aba80575e1fcf3ed0e127a60c4c9
NaviVani-dev <navivani.contact@gmail.com>
2026-09-14 15:43:27 -0500

    schema para la bd

--- backend/database/usuarios-correo-unico.sql
+++
@@ -1,4 +0,0 @@
--- Ejecútalo después de corregir cualquier correo duplicado existente.
-ALTER TABLE usuarios
-  MODIFY correo VARCHAR(255) NOT NULL,
-  ADD CONSTRAINT uq_usuarios_correo UNIQUE (correo);

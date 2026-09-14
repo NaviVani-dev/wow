649e1d7ae01ea2f2b783c534a513dccfe5bd1f57
Raulito2504 <raulariasrafael@gmail.com>
2026-09-13 21:04:01 -0500

    feat: conecta rutas de videojuegos al servidor

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
 import jugadoresRoutes from './routes/jugadores.routes.js';
+import videojuegosRoutes from './routes/videojuegos.routes.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/jugadores', jugadoresRoutes);
+app.use('/videojuegos', videojuegosRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
